54a51be948e311f0e1db9fbb009bced81bcedb7b
Ayden Scott <aydenscott444@gmail.com>
2024-03-15 21:23:31 +0000

    created first version of poetry archive! more search criteria needed for proper functionality... but you can look up shakespeare so that's a start

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
-require "sinatra"
+require 'sinatra'
 require "sinatra/reloader"
+require 'httparty'
+require 'json'
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+POETRYDB_API_BASE_URL = 'https://poetrydb.org'
+
+get '/' do
+  erb :index
+end
+
+get '/search' do
+  keyword = params[:keyword]
+  response = HTTParty.get("#{POETRYDB_API_BASE_URL}/author/#{keyword}")
+
+  if response.success?
+    @poems = JSON.parse(response.body)
+    erb :poems
+  else
+    @error_message = "Error: #{response.code}"
+    erb :error
+  end
 end
